ccaa661aa73f8dd66c8d17037ecc7c232ccb9c8e
nelisa-dludla <nelisa.dludla@outlook.com>
2023-07-10 21:08:56 +0200

    Add formatted version of the code for maze_bank_cli. Used Black for formatting.

--- maze_bank_cli/maze_bank_cli.py
+++ maze_bank_cli/maze_bank_cli.py
@@ -1,16 +1,19 @@
 import csv
-import random
-import sys
-import re
-import getpass
+from random import randint
+from sys import exit
+from re import match
+from getpass import getpass
+
 
 def main():
     # Welcome Page
-    print("""===== Welcome to Maze Bank =====
+    print(
+        """===== Welcome to Maze Bank =====
 
 (l) Log In
 (s) Sign Up
-""")
+"""
+    )
 
     while True:
         user_input = input("Input: ").strip().lower()
@@ -24,7 +27,10 @@ def main():
                 login()
                 break
         else:
-            print("Invalid Input. Enter (l) to Log In to existing account or (s) to Sign Up for a new account.\n")
+            print(
+                "Invalid Input. Enter (l) to Log In to existing account or (s) to Sign Up for a new account.\n"
+            )
+
 
 # For when user selects log in option
 def login():
@@ -32,12 +38,15 @@ def login():
 
     while True:
         username = input("Username: ").strip()
-        password = getpass.getpass("Password: ").strip()
+        password = getpass("Password: ").strip()
         valid_account = find_client_logins("clients.csv", username, password)
         # print(f"Both username and password are valid: {valid_account}")
         if valid_account:
-            account_number = get_value_from_csv("clients.csv", "username", username, "accountNumber")
+            account_number = get_value_from_csv(
+                "clients.csv", "username", username, "accountNumber"
+            )
             account_page(account_number)
+
 
 # For when user selects sign in option
 def signup():
@@ -57,18 +66,30 @@ def signup():
         # Phone Number
         phone_number = input("Enter your Phone Number: ").strip()
         while not is_valid_phone_number(phone_number):
-            print("Invalid Input. Phone Number should be 10 digits long.")
+            print("Invalid Input. Phone Number should be 11 digits long.")
             phone_number = input("Enter your Phone Number: ").strip()
         # Email
         email = input("Enter your email address: ").strip().lower()
         while not is_valid_email(email):
-            print("Invalid input. Make sure you are using the valid email address format.")
+            print(
+                "Invalid input. Make sure you are using the valid email address format."
+            )
             email = input("Enter your email address: ").strip().lower()
         # Account Type
-        account_type = input('Choose an account Type:\n(s) for "Savings" or (c) for "Cheque"\nInput: ').strip().lower()
+        account_type = (
+            input(
+                'Choose an account Type:\n(s) for "Savings" or (c) for "Cheque"\nInput: '
+            )
+            .strip()
+            .lower()
+        )
         while account_type not in ["s", "c"]:
             print("Invalid Input. Enter (s) to choose Savings or (c) to choose Cheque.")
-            account_type = input('Choose an account Type:\n(s) for "Savings" or (c) for "Cheque" ').strip().lower()
+            account_type = (
+                input('Choose an account Type:\n(s) for "Savings" or (c) for "Cheque" ')
+                .strip()
+                .lower()
+            )
 
         if account_type == "s":
             account_type = "Savings"
@@ -77,20 +98,25 @@ def signup():
         # Username
         username = input("Enter a Username: ").strip()
         while not is_valid_username(username):
-            print("Invalid Input. Username can only contain alphabets, numbers and/or an underscore.")
+            print(
+                "Invalid Input. Username can only contain alphabets, numbers and/or an underscore."
+            )
             username = input("Enter a Username: ")
         # Password
         password = input("Enter a Password: ").strip()
         while not is_valid_password(password):
-            print("""Invalid Input.
+            print(
+                """Invalid Input.
 Password must contain at least contain one alphabetical character (uppercase or lowercase)
 Password must contain at least one digit
 Password must be at least 8 characters long
-""")
+"""
+            )
             password = input("Enter a Password: ").strip()
         # Generate account number for client
         account_number = generate_account_number()
-        print(f"""\n===== Maze Bank - Account Details =====
+        print(
+            f"""\n===== Maze Bank - Account Details =====
 
 Your account has been created.
 
@@ -107,37 +133,61 @@ You can now log in using:
 
 username: {username}
 password: {password}
-""")
+"""
+        )
         # Write data into csv files
-        store_data_in_csv(first_name, last_name, phone_number, email, account_type, account_number, username, password)
+        store_data_in_csv(
+            first_name,
+            last_name,
+            phone_number,
+            email,
+            account_type,
+            account_number,
+            username,
+            password,
+        )
         # Exit sign in function
         break
 
+
 # To look for client log ins in clients.csv file
 def find_client_logins(file_name, username, password):
-    valid_username = False
-    valid_password = False
+    is_valid_username = False
+    is_valid_password = False
+    rows = []
+    # Open csv file and read its contents
     with open(file_name, "r") as csvfile:
-        csv_reader = csv.DictReader(csvfile)
+        csv_reader = csv.reader(csvfile)
+        header = next(csv_reader)
+        rows.append(header)
+        # Get index of relevant columns to match it with relevant inputs
+        username_index = header.index("username")
+        password_index = header.index("password")
         for row in csv_reader:
-            if row["username"] == username:
-                valid_username = True
+            if row[username_index] == username and row[password_index] == password:
+                is_valid_username = True
+                is_valid_password = True
+                break
+            elif row[username_index] == username and row[password_index] != password:
+                is_valid_username = True
+                break
+            elif row[username_index] != username and row[password_index] == password:
+                is_valid_password = True
+                break
 
-            if row["password"] == password:
-                valid_password = True
-
-    if valid_username and valid_password:
+    if is_valid_username and is_valid_password:
         return True
-    elif not valid_username and valid_password:
-            print("Incorrect Username or Username does not exist.")
-            return False
-    elif valid_username and not valid_password:
-            print("Incorrect Password.")
-            return False
+    elif not is_valid_username and is_valid_password:
+        print("Incorrect Username or Username does not exist.")
+        return False
+    elif is_valid_username and not is_valid_password:
+        print("Incorrect Password.")
+        return False
     else:
         print("Incorrect Username or Username does not exist.")
         print("Incorrect Password")
         return False
+
 
 # Displays client account page
 def account_page(value):
@@ -145,6 +195,7 @@ def account_page(value):
         account_balance = None
         client_name = None
         account_number = None
+        account_type = None
         # Retrieve client details
         with open("accounts.csv", "r") as csvfile:
             csv_reader = csv.DictReader(csvfile)
@@ -155,7 +206,8 @@ def account_page(value):
                     account_type = row["accountType"]
                     account_number = row["accountNumber"]
 
-        print(f"""\n===== Maze Bank - Account =====\n
+        print(
+            f"""\n===== Maze Bank - Account =====\n
 Welcome back, {client_name}
 
 Account Type: {account_type.title()}
@@ -167,7 +219,8 @@ Current Balance: {account_balance}
 (w) Withdraw
 
 (q) Log Out and Exit
-""")
+"""
+        )
         # Process User Input
         user_options = ["d", "t", "w", "q"]
         user_input = input("Input: ").strip().lower()
@@ -177,12 +230,13 @@ Current Balance: {account_balance}
             elif user_input == "t":
                 transfer(account_number)
             elif user_input == "q":
-                sys.exit("Logging Out...")
+                exit("Logging Out...")
             else:
                 withdraw(account_number)
         # User enters a character not in user_options
         else:
             print("Invalid Input.")
+
 
 # Displays deposit page and processes the accountBalance change
 def deposit(account_number):
@@ -195,12 +249,21 @@ def deposit(account_number):
             print("Invalid input. Make sure you have entered a value in numbers.")
         else:
             formatted_amount = format(amount, ".2f")
-            value = get_value_from_csv("accounts.csv", "accountNumber", account_number, "accountBalance")
+            value = get_value_from_csv(
+                "accounts.csv", "accountNumber", account_number, "accountBalance"
+            )
             new_amount = float(value) + amount
             formatted_new_amount = format(new_amount, ".2f")
-            change_value_in_csv("accounts.csv", "accountNumber", account_number, "accountBalance", formatted_new_amount)
+            change_value_in_csv(
+                "accounts.csv",
+                "accountNumber",
+                account_number,
+                "accountBalance",
+                formatted_new_amount,
+            )
             print(f"{formatted_amount} has been deposited into your account.")
             break
+
 
 # Function displays transfer page and processes the accountBalance change
 def transfer(account_number):
@@ -215,7 +278,9 @@ def transfer(account_number):
         else:
             formatted_transfer_amount = format(transfer_amount, ".2f")
             # Print verify tranaction page and get input from user
-            print(f"\n===== Maze Bank - Verify Transaction =====\n\nTransfer {formatted_transfer_amount} to account number {recipients_account}? (y/n)")
+            print(
+                f"\n===== Maze Bank - Verify Transaction =====\n\nTransfer {formatted_transfer_amount} to account number {recipients_account}? (y/n)"
+            )
             is_completed = False
             while not is_completed:
                 user_input = input("Input: ").strip().lower()
@@ -223,20 +288,46 @@ def transfer(account_number):
                 if user_input in user_options:
                     if user_input == "y" and is_valid_account(recipients_account):
                         # Find value of accountBalance for sender
-                        sender_balance = get_value_from_csv("accounts.csv", "accountNumber", account_number, "accountBalance")
-                        new_sender_balance = float(sender_balance) - float(formatted_transfer_amount)
+                        sender_balance = get_value_from_csv(
+                            "accounts.csv",
+                            "accountNumber",
+                            account_number,
+                            "accountBalance",
+                        )
+                        new_sender_balance = float(sender_balance) - float(
+                            formatted_transfer_amount
+                        )
                         new_sender_balance = format(new_sender_balance, ".2f")
                         # Check if sender has enough funds for transaction of requested amount
                         if float(new_sender_balance) < 0:
                             print("Insufficient funds. Transaction Cancelled.")
                             break
                         # Change value of accountBalance for sender
-                        change_value_in_csv("accounts.csv", "accountNumber", account_number, "accountBalance", new_sender_balance)
+                        change_value_in_csv(
+                            "accounts.csv",
+                            "accountNumber",
+                            account_number,
+                            "accountBalance",
+                            new_sender_balance,
+                        )
                         # Find and change value of accountBalance for recipient
-                        recipient_balance = get_value_from_csv("accounts.csv", "accountNumber", str(recipients_account), "accountBalance")
-                        new_recipient_balance = float(recipient_balance) + float(formatted_transfer_amount)
+                        recipient_balance = get_value_from_csv(
+                            "accounts.csv",
+                            "accountNumber",
+                            str(recipients_account),
+                            "accountBalance",
+                        )
+                        new_recipient_balance = float(recipient_balance) + float(
+                            formatted_transfer_amount
+                        )
                         new_recipient_balance = format(new_recipient_balance, ".2f")
-                        change_value_in_csv("accounts.csv", "accountNumber", str(recipients_account), "accountBalance", new_recipient_balance)
+                        change_value_in_csv(
+                            "accounts.csv",
+                            "accountNumber",
+                            str(recipients_account),
+                            "accountBalance",
+                            new_recipient_balance,
+                        )
                         is_completed = True
                         # Print success message
                         print("Transaction successful.")
@@ -252,9 +343,12 @@ def transfer(account_number):
                         is_completed = True
                         break
                 else:
-                    print("Invalid Input. Enter (y) for yes to confirm or (n) for no to cancel.\n")
+                    print(
+                        "Invalid Input. Enter (y) for yes to confirm or (n) for no to cancel.\n"
+                    )
             # Break out the main loop to take yo back to the account page
             break
+
 
 # Function displays withdraw page and processes the accountBalance change
 def withdraw(account_number):
@@ -269,7 +363,9 @@ def withdraw(account_number):
             # Format withdrawal amount to 2 decimal places
             formatted_amount = format(amount, ".2f")
             # Retrieve accountBalance from csv and deduct amount from current accountBalance
-            value = get_value_from_csv("accounts.csv", "accountNumber", account_number, "accountBalance")
+            value = get_value_from_csv(
+                "accounts.csv", "accountNumber", account_number, "accountBalance"
+            )
             new_value = float(value) - amount
             # Check if account has enough funds for transaction of requested amount
             if new_value < 0:
@@ -278,17 +374,31 @@ def withdraw(account_number):
                 # Format new amount to 2 decimal places
                 formatted_new_amount = format(new_value, ".2f")
                 # Change accountBalance to new amount
-                change_value_in_csv("accounts.csv", "accountNumber", account_number, "accountBalance", formatted_new_amount)
+                change_value_in_csv(
+                    "accounts.csv",
+                    "accountNumber",
+                    account_number,
+                    "accountBalance",
+                    formatted_new_amount,
+                )
                 print(f"{formatted_amount} has been withdrawn from your account.")
             break
 
+
 # Function generates an account number
 def generate_account_number():
-    account_number = random.randint(1000000000, 9999999999)
-    return account_number
+    account_number = randint(1000000000, 9999999999)
+    while True:
+        if is_available("accounts.csv", str(account_number), "accountNumber"):
+            return account_number
+        else:
+            account_number = randint(1000000000, 9999999999)
+
 
 # Function changes the value inside a csv file
-def change_value_in_csv(filename, condition_column, condition_value, target_column_name, new_value):
+def change_value_in_csv(
+    filename, condition_column, condition_value, target_column_name, new_value
+):
     rows = []
     # Open csv file and read its contents
     with open(filename, "r") as csvfile:
@@ -310,6 +420,7 @@ def change_value_in_csv(filename, condition_column, condition_value, target_colu
         csv_writer = csv.writer(csvfile)
         csv_writer.writerows(rows)
 
+
 # Function retrieves a value from csv file
 def get_value_from_csv(filename, condition_column, condition_value, target_column):
     # Open csv file and read its contents
@@ -319,6 +430,7 @@ def get_value_from_csv(filename, condition_column, condition_value, target_colum
         for row in csv_reader:
             if row[condition_column] == condition_value:
                 return row[target_column]
+
 
 # Function checks if account number exists
 def is_valid_account(account_number):
@@ -332,32 +444,48 @@ def is_valid_account(account_number):
             elif row["accountNumber"] == None:
                 return False
 
+
 # Function checks if email is in the correct format
 def is_valid_email(email):
     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    return re.match(pattern, email) is not None
+    return match(pattern, email) is not None
+
 
 # Function checks if first/last name only contains alphabets
 def is_valid_first_last(name):
     pattern = r"^[a-zA-Z].+$"
-    return re.match(pattern, name) is not None
+    return match(pattern, name) is not None
+
 
 # Function checks if username contains valid characters
 def is_valid_username(username):
     pattern = r"^[a-zA-Z0-9_]+$"
-    return re.match(pattern, username) is not None
+    return match(pattern, username) is not None
+
 
 # Function checks if password meets requirements
 def is_valid_password(password):
     pattern = r"^^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-    return re.match(pattern, password) is not None
+    return match(pattern, password) is not None
+
 
 # Function checks if the phone number only contains numbers and is 10 digits long
 def is_valid_phone_number(phone_number):
-    pattern = r"^[0-9]{,10}$"
-    return re.match(pattern, phone_number) is not None
+    pattern = r"^[0-9]{,11}$"
+    return match(pattern, phone_number) is not None
 
-def store_data_in_csv(firstName, lastName, phoneNumber, email, accountType, accountNumber, username, password):
+
+# Function stores data of new sign up into relevant csv files
+def store_data_in_csv(
+    firstName,
+    lastName,
+    phoneNumber,
+    email,
+    accountType,
+    accountNumber,
+    username,
+    password,
+):
     # clients.csv
     with open("clients.csv", "r") as csvfile:
         # List to store contents from clients.csv
@@ -402,6 +530,16 @@ def store_data_in_csv(firstName, lastName, phoneNumber, email, accountType, acco
     with open("accounts.csv", "w") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerows(rows)
+
+
+# Function checks if value is already taken
+def is_available(filename, value, column):
+    with open(filename, "r") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            if row[column] == value:
+                return False
+        return True
 
 
 if __name__ == "__main__":
